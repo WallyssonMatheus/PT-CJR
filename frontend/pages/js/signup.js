7172eb1f2e2892b138643504326ef423e7f1700d
Savio Henrique <saviohc@pop-os.localdomain>
2023-11-22 14:09:34 -0300

    :sparkle: feat: Added 2 pages, feed_logado and feed_deslogado, WIP js

---
+++ frontend/pages/js/signup.js
@@ -0,0 +1,30 @@
+async function signup(event){
+    event.preventDefault();
+    const data = new FormData();
+    
+    // Getting the data
+    const perfil = document.getElementById("perfil").files[0];
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const nome = document.getElementById("nome").value;
+    const genero = document.getElementById("genero").value;
+    const cargo = document.getElementById("cargo").value;
+    const blob = new Blob([perfil], {'type': perfil.type});
+    
+    console.log(blob)
+    console.log(perfil,email,senha,nome,genero,cargo);
+
+    data.append("name",nome);
+    data.append("email",email);
+    data.append("password",senha);
+    data.append("gender",genero);
+    data.append("job_title",cargo);
+    data.append("profile_picture",perfil);
+
+    console.log(data);
+
+    const response = await fetch("")
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", signup);

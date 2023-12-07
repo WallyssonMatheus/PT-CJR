83f966379e1b20c90d64f83869b81672a56317fd
Savio Henrique <83360232+savio-henrique@users.noreply.github.com>
2023-12-06 21:55:38 -0300

    :wrench: fix: Removed console.logs

--- backend/frontend/pages/js/isAuth.js
+++ backend/frontend/pages/js/isAuth.js
@@ -1,8 +1,6 @@
 function auth(){
     const isAuth = sessionStorage.getItem("isAuth");
     const url = window.location.href;
-    console.log(isAuth);
-    console.log(url);
     if (isAuth == "true" && url !== "http://localhost:3000/feed_logado.html")  {
         window.location.href = "http://localhost:3000/feed_logado.html";
     }
